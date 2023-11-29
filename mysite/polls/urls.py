f37267879a2581545253ef805cc73da7ce86b5e3
LakhAlex <blackmoonstone@daum.net>
2023-11-29 23:12:32 +0900

    commit_2023/11/29

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,9 +6,8 @@ from . import views  # ' . '은 현재 디렉토리를 의미, 그러니까 poll
 app_name = 'polls'
 
 urlpatterns = [
-    # path('', views.name, name='name'),
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,53 +1,42 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from polls.models import Question, Choice  # Question이라는 이름의 테이블을 import하기
-from polls.forms import NameForm
+from django.views.generic import ListView
+from django.views.generic import DetailView
+
 
 # Create your views here.
 
-def index(request):
-    lastest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {'lastest_question_list' : lastest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    def get_queryset(self):
+        # 최근 생성된 5개의 질문을 반환한다
+        queryset = Question.objects.order_by('-pub_date')[:5]
+        print(queryset)  # Question에 데이터가 정상적으로 들어가 있는지 확인용
+        return queryset
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
+class ResultView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk = question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except(KeyError, Choice.DoesNotExist):
-        # 설문 투표 폼을 다시 보여 준다.
-        return render(request, 'polls/detail.html',
-                      {'question':question, 'error_message': "You didn't select a choice",})
+        selected_chocie = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question' : question,
+            'error_message' : "You didn't select a choice"
+        })
     else:
-        selected_choice.votes += 1
-        selected_choice.save() # DB에 저장
-        # POST 데이터를 정상적으로 처리하였으면,
-        # 항상 HttpResponseRedirect를 반환하여 리다이렉션을 처리함
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-def name(request):
-    if request.method == 'POST':
-        form = NameForm(request.POST)
-
-        if form.is_valid(): # 폼에 담긴 데이터가 유효한지 체크
-            new_name = form.cleaned_data['name']
-            return HttpResponseRedirect('')
-    else:
-        form = NameForm()
-    return render(request, 'polls/name.html', {'form':form})
-
-
-# -------------------
-
-
-
+        selected_chocie.votes += 1
+        selected_chocie.save()
+        return redirect('polls:results')
